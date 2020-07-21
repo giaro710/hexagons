f94e3f0f48252f12ddd15278a3ef4616db44ede5
Gian Marco Romani <giaro710@gmail.com>
2020-07-21 12:59:58 +0200

    some style

--- app/models/hexagon.rb
+++ app/models/hexagon.rb
@@ -2,7 +2,7 @@ class Hexagon < ApplicationRecord
   belongs_to :path
 
   validates :title, presence: true
-  validates :title, length: { in: 1..30 }
+  validates :title, length: { in: 1..21 }
 
   has_rich_text :long_content
 end
